7cc28f531fe454d2ccb83a8825e3192bb739b999
Soumya Mondal <s.mondal8@gmail.com>
2020-05-24 15:32:15 +0530

    rectification of problem

--- src/Algorithm/InterviewBitProblemNumRange.java
+++ src/Algorithm/InterviewBitProblemNumRange.java
@@ -3,20 +3,23 @@ package Algorithm;
 import java.util.Arrays;
 //https://www.interviewbit.com/problems/numrange/
 public class InterviewBitProblemNumRange {
-        // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
         public int numRange(int[] A, int B, int C) {
             int start = 0;
             int offset = 1;
             int result = 0;
             int sum = 0;
-            while(start < A.length){
+            while(start < A.length-1){
                 sum = A[start];
-                while (offset < A.length){
-                    if (sum+A[start+offset]<=C && sum+A[start+offset]>=B){
+                offset = 1;
+                while (offset+start < A.length){
+                    if (sum+A[start+offset]<C && sum+A[start+offset]>B){
                         result++;
                         sum = sum+A[start+offset];
-                        offset++;
                     }
+                    else if(sum+A[start+offset]>=C){
+                        break;
+                    }
+                    offset++;
                 }
                 start++;
             }
